5b049068242b48f9d9128b5bb5a15169c889ba87
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-07-23 14:29:39 +0530

    Create Implement strstr.java

---
+++ SOTI/Implement strstr.java
@@ -0,0 +1,9 @@
+class GfG
+{
+    //Function to locate the occurrence of the string x in the string s.
+    int strstr(String s, String x)
+    {
+       // Your code here
+       return s.indexOf(x);
+    }
+}
